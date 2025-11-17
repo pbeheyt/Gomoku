3dcf2ca6e0af68bb346ac54371582a760d2e03c0
pbeheyt <pierre.beheyt.bba@edhec.com>
2025-11-17 19:12:58 +0100

    Full screen size on first render

--- src/main/index.ts
+++ src/main/index.ts
@@ -3,13 +3,17 @@ import path from 'path';
 
 function createWindow() {
   const mainWindow = new BrowserWindow({
-    width: 1200,
-    height: 900,
+    show: false,
+    minWidth: 1280,
+    minHeight: 800,
     webPreferences: {
       preload: path.join(__dirname, '../renderer/preload.js'),
       contextIsolation: true,
     },
   });
+
+  mainWindow.maximize();
+  mainWindow.show();
 
   mainWindow.loadFile(path.join(__dirname, '../renderer/index.html'));
 }
