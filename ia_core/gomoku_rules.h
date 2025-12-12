bbbc0cb1693f55b17813e96286700e6df92f7c80
pbeheyt <pierre.beheyt.bba@edhec.com>
2025-12-12 19:16:07 +0100

    rm .h rm function

--- ia_core/gomoku_rules.h
+++ ia_core/gomoku_rules.h
@@ -99,7 +99,6 @@ private:
     
     // Helpers d'Arbitrage (Victoire Cassable)
     static bool isLineBreakableByCapture(const int board[BOARD_SIZE][BOARD_SIZE], const std::vector<Point>& line, int opponent);
-    static bool doesCaptureBreakWin(int lineLength, int removeIdx1, int removeIdx2);
     static bool isPairSandwiched(const int board[BOARD_SIZE][BOARD_SIZE], Point p1, Point p2, int opponent);
     static bool tryCaptureAt(const int board[BOARD_SIZE][BOARD_SIZE], int r, int c, int opponent);
     static bool isStoneCapturable(const int board[BOARD_SIZE][BOARD_SIZE], int row, int col, int opponent);
